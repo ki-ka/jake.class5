d3ffea684a0f79f1d21a73ec063809ef206b6ac8
ki-ka <100897171+ki-ka@users.noreply.github.com>
2022-03-18 17:24:26 +0900

    section6,~29

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondalyButton } from "./components/atoms/button/SecondalyButton";
+import { SearchInput } from "./components/molecules/SeachInput";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
+      <SecondalyButton>検索</SecondalyButton>
+      <br />
+      <SearchInput />
     </div>
   );
 }
