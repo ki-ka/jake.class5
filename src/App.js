f6771cd365450cbab631d88556b7baac6526a832
ki-ka <100897171+ki-ka@users.noreply.github.com>
2022-03-22 10:54:32 +0900

    -32

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
+import { BrowserRouter } from "react-router-dom";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondalyButton } from "./components/atoms/button/SecondalyButton";
 import { SearchInput } from "./components/molecules/SeachInput";
+import { UserCard } from "./components/organism/user/UserCard";
+import { DefaultLayout } from "./components/templates/DefaultLayout";
+import { HeaderOnly } from "./components/templates/HeaderOnly";
 import "./styles.css";
+
+const user = {
+  name: "じゃけぇ",
+  image: "https://source.unsplash.com/Mv9hjnEUHR4",
+  email: "eeeeeee@ee.com",
+  phone: "000-0000-0000",
+  company: {
+    name: "ｗｗｗ会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
-    <div className="App">
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondalyButton>検索</SecondalyButton>
-      <br />
-      <SearchInput />
-    </div>
+    <BrowserRouter>
+      <DefaultLayout>
+        <PrimaryButton>テスト</PrimaryButton>
+        <SecondalyButton>検索</SecondalyButton>
+        <br />
+        <SearchInput />
+        <UserCard user={user} />
+      </DefaultLayout>
+    </BrowserRouter>
   );
 }
